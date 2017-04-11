8b65e48c6ef9f8e42145af0fa72eecb4628eb585
Ssuarez0 <ssuarez@2011.nhi.edu>
2017-04-11 11:51:56 -0700

    Apps, settings and views update
    -added the bird app skeleton
    -created some skeleton views in views.py of birds
    -created some template directories
    -renamed project folders to be more reasonable
    -updated settings
    
    Still a lot to do, including a skeleton birds model and base index.

--- StanislausBirds/StanislausBirds/wsgi.py
+++ StanislausBirds/StanislausBirds/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Birds project.
+WSGI config for StanislausBirds project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Birds.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StanislausBirds.settings")
 
 application = get_wsgi_application()
---
+++ StanislausBirds/apps/birds/views.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from apps.birds.models import Bird
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render_to_response
+
+class BirdList(ListView):
+    model = Bird
+    template_name = ''
+    queryset = ''#Will be changed once models are ironed out
+
+    def get_queryset(self):
+        return self.queryset
+
+class BirdEntry(DetailView):
+    model = Bird
+    template_name = ''
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
