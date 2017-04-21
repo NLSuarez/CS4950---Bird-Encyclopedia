59cc1d0a4375d1bcb07a145476ecb10fd93410bf
SSuarez0 <ssuarez@2011.nhi.edu>
2017-04-21 10:45:19 -0700

    Updated Database Schema
    I kept running into issues remembering the field names when writing the
    program to upload data to the database, so I altered them to be easier to
    remember. Still working on uploading the proper data.

--- StanislausBirds/apps/birds/models.py
+++ StanislausBirds/apps/birds/models.py
@@ -4,17 +4,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Bird(models.Model):
-    Common_Name = models.CharField(max_length=100, unique=True)
-    Species_Name = models.CharField(max_length=100, unique=True)
-    Short_Description = models.TextField()
-    Image_Ref = models.ImageField(blank=True, null=True) #requires pillow (pip install pillow)
-    Sound_Ref = models.FileField(blank=True, null=True)
+    Name = models.CharField(max_length=100, unique=True)
+    Species = models.CharField(max_length=100, unique=True)
+    Description = models.TextField()
+    Image = models.ImageField(blank=True, null=True) #requires pillow (pip install pillow)
+    Sound = models.FileField(blank=True, null=True)
     Diet = models.TextField(blank=True, null=True)
-    Sleeping_Habits = models.CharField(max_length=35, blank=True, null=True)
+    Sleep = models.CharField(max_length=35, blank=True, null=True)
     Habitat = models.TextField(blank=True, null=True)
 
     class Meta:
-        ordering = ('Common_Name', )
+        ordering = ('Name', )
 
     def __unicode__(self):
-        return u'{}'.format(self.Common_Name)
+        return u'{}'.format(self.Name)
