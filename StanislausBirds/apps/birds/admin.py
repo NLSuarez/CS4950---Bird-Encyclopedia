7fbe08101043df5fab8fc5f5e1f97269bf0853d6
Stefan Suarez <ssuarez@2011.nhi.edu>
2017-04-27 15:02:54 -0700

    Test Compatibility With Windows
    In order to make it easier to present this website, I'm going to try
    programming this on Windows. However, I'd like to know if this breaks on
    Linux. It shouldn't because of the limited level of changes, but we'll
    see.

--- StanislausBirds/apps/birds/admin.py
+++ StanislausBirds/apps/birds/admin.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.contrib import admin
 
 from .models import Bird
 
-admin.site.register(Bird)
+@admin.register(Bird)
+class BirdAdmin(admin.ModelAdmin):
+    pass
--- StanislausBirds/apps/birds/models.py
+++ StanislausBirds/apps/birds/models.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+#from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
+@python_2_unicode_compatible
 class Bird(models.Model):
     Name = models.CharField(max_length=100, unique=True)
     Species = models.CharField(max_length=100, unique=True)
@@ -16,5 +17,7 @@ class Bird(models.Model):
     class Meta:
         ordering = ('Name', )
 
-    def __unicode__(self):
-        return u'{}'.format(self.Name)
+    def __str__(self):
+        return '%s' % (self.Name)
+    #def __unicode__(self):
+        #return u'{}'.format(self.Name)
