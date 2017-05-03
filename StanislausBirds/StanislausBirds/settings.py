90cbc593a4a4424cb4a0f95b366d02fafc966e62
Stefan Suarez <ssuarez@2011.nhi.edu>
2017-05-02 19:32:22 -0700

    Bootstrap, jQuery and Base Template
    Bootstrap and jQuery have been added as static files so they can be
    used even without internet. The necessary markup to include them has
    also been added into a base.html file. By extending all other templates
    from that, we can access bootstrap and jquery functionality from anywhere
    in the application.

--- StanislausBirds/StanislausBirds/settings.py
+++ StanislausBirds/StanislausBirds/settings.py
@@ -119,5 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.abspath(os.path.join(BASE_DIR, 'StanislausBirds/static/')),
+)
